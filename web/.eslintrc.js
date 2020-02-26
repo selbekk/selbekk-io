c45bf20b9c84c8de51e9e0e283450ce76f8c8b6b
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-02-26 20:52:35 +0100

    Set up eslint with more modern standards

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,13 +1,3 @@
 module.exports = {
-  extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
-  rules: {
-    'react/prop-types': 0,
-    'object-curly-spacing': ['error', 'never']
-  },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: '16.8.4'
-    }
-  }
-}
+  extends: ["react-app"]
+};
