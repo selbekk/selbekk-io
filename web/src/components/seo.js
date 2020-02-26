acd48e740fa8bce33eab745c8f469f6606c96b40
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-02-26 21:40:08 +0100

    Refactor the SEO component to use hooks

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -1,82 +1,88 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet-async';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 import { imageUrlFor } from '../lib/image-url';
 import { buildImageObj } from '../lib/helpers';
 
-function SEO({ description, lang, meta, keywords, title, image }) {
-  return (
-    <StaticQuery
-      query={detailsQuery}
-      render={(data) => {
-        const metaDescription =
-          description || (data.site && data.site.description) || '';
-        const siteTitle = (data.site && data.site.title) || '';
-        const siteAuthor =
-          (data.site && data.site.author && data.site.author.name) || '';
-        const metaImage =
-          image && image.asset
-            ? imageUrlFor(buildImageObj(image))
-                .width(1200)
-                .url()
-            : '';
+const detailsQuery = graphql`
+  query DefaultSEOQuery {
+    site: sanitySiteSettings(_id: { eq: "siteSettings" }) {
+      title
+      description
+      keywords
+      author {
+        name
+      }
+    }
+  }
+`;
 
-        return (
-          <Helmet
-            htmlAttributes={{ lang }}
-            title={title}
-            titleTemplate={title === siteTitle ? '%s' : `%s | ${siteTitle}`}
-            meta={[
-              {
-                name: 'description',
-                content: metaDescription,
-              },
-              {
-                property: 'og:title',
-                content: title,
-              },
-              {
-                property: 'og:description',
-                content: metaDescription,
-              },
-              {
-                property: 'og:type',
-                content: 'website',
-              },
-              {
-                property: 'og:image',
-                content: metaImage,
-              },
-              {
-                name: 'twitter:card',
-                content: 'summary',
-              },
-              {
-                name: 'twitter:creator',
-                content: siteAuthor,
-              },
-              {
-                name: 'twitter:title',
-                content: title,
-              },
-              {
-                name: 'twitter:description',
-                content: metaDescription,
-              },
-            ]
-              .concat(
-                keywords && keywords.length > 0
-                  ? {
-                      name: 'keywords',
-                      content: keywords.join(', '),
-                    }
-                  : [],
-              )
-              .concat(meta)}
-          />
-        );
-      }}
+function SEO({ description, lang, meta, keywords, title, image }) {
+  const { site } = useStaticQuery(detailsQuery);
+
+  const metaDescription = description || (site && site.description) || '';
+  const siteTitle = (site && site.title) || '';
+  const siteAuthor = (site && site.author && site.author.name) || '';
+  const metaImage =
+    image && image.asset
+      ? imageUrlFor(buildImageObj(image))
+          .width(1200)
+          .url()
+      : '';
+
+  return (
+    <Helmet
+      htmlAttributes={{ lang }}
+      title={title}
+      titleTemplate={title === siteTitle ? '%s' : `%s | ${siteTitle}`}
+      meta={[
+        {
+          name: 'description',
+          content: metaDescription,
+        },
+        {
+          property: 'og:title',
+          content: title,
+        },
+        {
+          property: 'og:description',
+          content: metaDescription,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          property: 'og:image',
+          content: metaImage,
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          name: 'twitter:creator',
+          content: siteAuthor,
+        },
+        {
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          name: 'twitter:description',
+          content: metaDescription,
+        },
+      ]
+        .concat(
+          keywords && keywords.length > 0
+            ? {
+                name: 'keywords',
+                content: keywords.join(', '),
+              }
+            : [],
+        )
+        .concat(meta)}
     />
   );
 }
@@ -96,16 +102,3 @@ SEO.propTypes = {
 };
 
 export default SEO;
-
-const detailsQuery = graphql`
-  query DefaultSEOQuery {
-    site: sanitySiteSettings(_id: { eq: "siteSettings" }) {
-      title
-      description
-      keywords
-      author {
-        name
-      }
-    }
-  }
-`;
