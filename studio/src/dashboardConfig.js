7dc34d629fffa7c1ff66d55768120c3b8080a433
Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
2020-02-26 20:02:37 +0100

    Initial commit

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,47 @@
+export default {
+  widgets: [
+    { name: 'structure-menu' },
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5e56c0c7ea21a2018ddd812f',
+                  title: 'Sanity Studio',
+                  name: 'selbekk-io-studio-87tt6tqi',
+                  apiId: 'f66f404a-f7c2-4987-91e2-dfd46295fae6'
+                },
+                {
+                  buildHookId: '5e56c0c78049b901c2a759d9',
+                  title: 'Blog Website',
+                  name: 'selbekk-io',
+                  apiId: '3218f88b-8098-4201-8891-b77fa55f429b'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/selbekk/selbekk-io',
+            category: 'Code'
+          },
+          { title: 'Frontend', value: 'https://selbekk-io.netlify.com', category: 'apps' }
+        ]
+      }
+    },
+    { name: 'project-users', layout: { height: 'auto' } },
+    {
+      name: 'document-list',
+      options: { title: 'Recent blog posts', order: '_createdAt desc', types: ['post'] },
+      layout: { width: 'medium' }
+    }
+  ]
+}
---
+++ web/client-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  sanity: {
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'topo6k9s',
+    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
+  }
+}
