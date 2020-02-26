8754417e30b652ded79e533812a1e65c8e906ec3
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-02-26 21:01:16 +0100

    Use react-helmet-async instead

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,24 +1,24 @@
 // Load variables from `.env` as soon as possible
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV || 'development'}`
-})
+  path: `.env.${process.env.NODE_ENV || 'development'}`,
+});
 
-const clientConfig = require('./client-config')
+const clientConfig = require('./client-config');
 
-const isProd = process.env.NODE_ENV === 'production'
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
-    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-react-helmet-async',
     {
       resolve: 'gatsby-source-sanity',
       options: {
         ...clientConfig.sanity,
         token: process.env.SANITY_READ_TOKEN,
         watchMode: !isProd,
-        overlayDrafts: !isProd
-      }
-    }
-  ]
-}
+        overlayDrafts: !isProd,
+      },
+    },
+  ],
+};
