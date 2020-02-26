394e19e3a4a2337a662e8b3220f5f9e425749944
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-02-26 22:15:25 +0100

    Remove the archive link in the header

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -18,14 +18,6 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
       >
         <Icon symbol="hamburger" />
       </button>
-
-      <nav className={cn(styles.nav, showNav && styles.showNav)}>
-        <ul>
-          <li>
-            <Link to="/archive/">Archive</Link>
-          </li>
-        </ul>
-      </nav>
     </div>
   </div>
 );
