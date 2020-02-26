95be3d5dce05a0aa9e9275558f8051ef59c1ae9e
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-02-26 21:40:15 +0100

    Change the 404 page a bit

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from '../components/seo';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Oops, that page ain't around no mo'</h1>
+    <p>Go back to the front page and try again!</p>
   </Layout>
 );
 
