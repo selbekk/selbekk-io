d2b62d355002a64ed17ce601f8987082f223804f
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-02-28 09:51:50 +0100

    Add support for bash language also

--- web/src/components/CodeBlock.js
+++ web/src/components/CodeBlock.js
@@ -4,17 +4,23 @@ import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 import tsx from 'react-syntax-highlighter/dist/esm/languages/prism/tsx';
 import elm from 'react-syntax-highlighter/dist/esm/languages/prism/elm';
+import bash from 'react-syntax-highlighter/dist/esm/languages/prism/bash';
 import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
 
 SyntaxHighlighter.registerLanguage('js', js);
 SyntaxHighlighter.registerLanguage('jsx', jsx);
 SyntaxHighlighter.registerLanguage('tsx', tsx);
-SyntaxHighlighter.registerLanguage('elm', js);
+SyntaxHighlighter.registerLanguage('elm', elm);
+SyntaxHighlighter.registerLanguage('bash', bash);
 
 export const CodeBlock = ({ node }) => {
   if (!node || !node.code) {
     return null;
   }
   const { language, code } = node;
-  return <SyntaxHighlighter language={language}>{code}</SyntaxHighlighter>;
+  return (
+    <SyntaxHighlighter style={prism} language={language}>
+      {code}
+    </SyntaxHighlighter>
+  );
 };
