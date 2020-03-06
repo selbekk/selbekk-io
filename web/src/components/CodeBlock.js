230e07da0b858baec159951076fba51924d569c6
Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>
2020-03-06 19:32:42 +0100

    Add support for CSS and HTML highlighting

--- web/src/components/CodeBlock.js
+++ web/src/components/CodeBlock.js
@@ -3,10 +3,14 @@ import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import js from 'react-syntax-highlighter/dist/esm/languages/prism/javascript';
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx';
 import tsx from 'react-syntax-highlighter/dist/esm/languages/prism/tsx';
+import css from 'react-syntax-highlighter/dist/esm/languages/prism/css';
+import html from 'react-syntax-highlighter/dist/esm/languages/prism/markup';
 import elm from 'react-syntax-highlighter/dist/esm/languages/prism/elm';
 import bash from 'react-syntax-highlighter/dist/esm/languages/prism/bash';
 import prism from 'react-syntax-highlighter/dist/esm/styles/prism/prism';
 
+SyntaxHighlighter.registerLanguage('html', html);
+SyntaxHighlighter.registerLanguage('css', css);
 SyntaxHighlighter.registerLanguage('js', js);
 SyntaxHighlighter.registerLanguage('jsx', jsx);
 SyntaxHighlighter.registerLanguage('tsx', tsx);
