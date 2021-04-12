bb6887b06ce1c2b4fefe66c19019cfe4b0b64bf6
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-04-12 11:40:26 +0200

    Delete all the old web code

--- web/README.md
+++
@@ -1,5 +0,0 @@
-# selbekk.io
-
-This is the Gatsby site that generates the blog.
-
-Start it locally with `npm run dev` and it'll pop open on `localhost:8000`.
--- web/client-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'topo6k9s',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
-  }
-}
