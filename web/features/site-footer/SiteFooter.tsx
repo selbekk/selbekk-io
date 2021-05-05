cef41a16e72bcb6eb97da96259f81a3c4e866663
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2021-05-05 07:50:30 +0200

    Tweak footer

--- web/features/site-footer/SiteFooter.tsx
+++ web/features/site-footer/SiteFooter.tsx
@@ -1,20 +1,15 @@
-import { Box, BoxProps, Stack, Text } from "@chakra-ui/react";
+import { Box, BoxProps, Text } from "@chakra-ui/react";
 import * as React from "react";
 import { LightSwitch } from "../light-switch/LightSwitch";
 
 export const SiteFooter = (props: BoxProps) => {
   return (
     <Box {...props}>
-      <Stack p={6} textAlign="center" direction={["column", "row"]}>
-        <Box flex="1">
-          <LightSwitch />
-        </Box>
-        <Box flex="1">
-          <Text>
-            All rights reserved &copy; {new Date().getFullYear()}
-          </Text>
-        </Box>
-      </Stack>
+      <Box p={6} maxWidth="xl" mx="auto" textAlign="center">
+        <Text>
+          All rights reserved &copy; {new Date().getFullYear()} <LightSwitch />
+        </Text>
+      </Box>
     </Box>
   );
 };
