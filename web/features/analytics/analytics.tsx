01ff40a8f0107ffc1ee27092992968b77806057d
Kristofer Giltvedt Selbekk <selbeezy@gmail.com>
2022-04-08 15:29:08 +0200

    Add plausible for tracking users

---
+++ web/features/analytics/analytics.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const AnalyticsScript = () => (
+  <script
+    defer
+    data-domain="selbekk.io"
+    src="https://plausible.io/js/plausible.js"
+  />
+);
--- web/pages/_document.tsx
+++ web/pages/_document.tsx
@@ -1,11 +1,14 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import NextDocument, { Head, Html, Main, NextScript } from "next/document";
+import { AnalyticsScript } from "../features/analytics/analytics";
 import { theme } from "../lib/theme";
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <AnalyticsScript />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
